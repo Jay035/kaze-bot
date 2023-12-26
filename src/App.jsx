467f64956475b1bc0c54edff552611d40b04eb6e
Jay035 <jaydencolins08@gmail.com>
2023-12-26 11:58:10 +0100

    added page load animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import { Features } from "./components/Features";
 import Footer from "./components/Footer";
 import { HeroSection } from "./components/HeroSection";
+import Loader from "./components/Loader";
 import Navbar from "./components/Navbar";
 import Trade from "./components/Trade";
 import Why from "./components/Why";
 
 function App() {
+  const [pageLoaded, setPageLoaded] = useState(false);
+  window.addEventListener("load", () => {
+    setTimeout(() => setPageLoaded(true), 2000);
+  });
   return (
     <div className="bg-[#010101] relative aspekta-regular">
-      <Navbar />
-      <HeroSection />
-      <Features />
-      <Why />
-      <Trade />
-      <Footer />
+      {pageLoaded ? (
+        <>
+          {" "}
+          <Navbar />
+          <HeroSection />
+          <Features />
+          <Why />
+          <Trade />
+          <Footer />
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,77 @@
-import logo from "../assets/Logo.svg";
+// import logo from "../assets/Logo.svg";
 
 export default function Loader() {
   return (
     <div className="flex justify-center items-center min-h-screen animate-pulse">
-        <img className="w-24" src={logo} alt="logo" />
+      <svg
+        width="81"
+        height="70"
+        viewBox="0 0 81 70"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M56.8108 6.0592H24.3989L26.8613 11.1186H54.7485L56.8108 6.0592Z"
+          fill="#69FF77"
+          stroke="#69FF77"
+          strokeWidth="0.158107"
+        />
+        <path
+          d="M20.678 1L0.999817 34.5187H6.99287L23.7672 6.17793L20.678 1Z"
+          fill="#69FF77"
+          stroke="#69FF77"
+          strokeWidth="0.158107"
+        />
+        <path
+          d="M60.8271 1L80.3687 34.5187H74.4172L57.7593 6.17793L60.8271 1Z"
+          fill="#69FF77"
+          stroke="#69FF77"
+          strokeWidth="0.158107"
+        />
+        <path
+          d="M13.3267 35.1511H7.32385L24.0088 63.9266L27.0872 58.7618L13.3267 35.1511Z"
+          fill="#69FF77"
+          stroke="#69FF77"
+          strokeWidth="0.158107"
+        />
+        <path
+          d="M68.0417 35.1511H74.0446L57.3596 63.9266L54.2812 58.7618L68.0417 35.1511Z"
+          fill="#69FF77"
+          stroke="#69FF77"
+          strokeWidth="0.158107"
+        />
+        <path
+          d="M57.5605 64.0846H23.6899L20.9212 69.3021H60.6061L57.5605 64.0846Z"
+          fill="#69FF77"
+          stroke="#69FF77"
+          strokeWidth="0.158107"
+        />
+        <path
+          d="M27.0077 24.9536L34.7549 34.9144"
+          stroke="#69FF77"
+          strokeWidth="4.74321"
+        />
+        <path
+          d="M34.7549 24.9536L27.0077 34.9144"
+          stroke="#69FF77"
+          strokeWidth="4.74321"
+        />
+        <path
+          d="M46.9749 50.866L34.3598 50.5596"
+          stroke="#69FF77"
+          strokeWidth="4.74321"
+        />
+        <path
+          d="M47.1669 24.9536L54.9142 34.9144"
+          stroke="#69FF77"
+          strokeWidth="4.74321"
+        />
+        <path
+          d="M54.9141 24.9536L47.1669 34.9144"
+          stroke="#69FF77"
+          strokeWidth="4.74321"
+        />
+      </svg>
     </div>
-  )
+  );
 }
