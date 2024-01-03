0a850404afbe9a624019c166a44a927813b9b64e
Jay035 <jaydencolins08@gmail.com>
2024-01-03 12:05:46 +0100

    implemented pagination functionality

--- app/leaderboard/components/LeaderboardTable.tsx
+++ app/leaderboard/components/LeaderboardTable.tsx
@@ -1,222 +1,253 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import "../../pagination.css";
 
 type Props = {
-	username: string;
-	referrals: number;
-	points: number;
+  username: string;
+  referrals: number;
+  points: number;
 };
 
 function sortByReferral(data: any) {
-	return data.sort(
-		(a: any, b: any) =>
-			parseInt(b.referralCount) - parseInt(a.referralCount)
-	);
+  return data.sort(
+    (a: any, b: any) => parseInt(b.referralCount) - parseInt(a.referralCount)
+  );
 }
 function sortByRoyaPoints(data: any) {
-	return data.sort(
-		(a: any, b: any) => parseInt(b.points) - parseInt(a.points)
-	);
+  return data.sort((a: any, b: any) => parseInt(b.points) - parseInt(a.points));
 }
 function sortByContestPoints(data: any) {
-	//console.log(first)
-	const d = data.sort(
-		(a: any, b: any) => parseInt(b.contestP) - parseInt(a.contestP)
-	);
-	return d.filter((el: any) => parseFloat(el.contestP) > 0);
+  //console.log(first)
+  const d = data.sort(
+    (a: any, b: any) => parseInt(b.contestP) - parseInt(a.contestP)
+  );
+  return d.filter((el: any) => parseFloat(el.contestP) > 0);
 }
 
 //
 export default function LeaderboardTable({ data }: any) {
-	const [userData, setUserData]: any = useState();
-	const [isRoyaltyPointInAscendingOrder, setIsRoyaltyPointInAscendingOrder] =
-		useState(false);
-	const [isContestPointInAscendingOrder, setIsContestPointInAscendingOrder] =
-		useState(false);
-	const [isReferralInAscendingOrder, setIsReferralInAscendingOrder] =
-		useState(false);
+  const [userData, setUserData]: any = useState();
+  const [isRoyaltyPointInAscendingOrder, setIsRoyaltyPointInAscendingOrder] =
+    useState(false);
+  const [isContestPointInAscendingOrder, setIsContestPointInAscendingOrder] =
+    useState(false);
+  const [isReferralInAscendingOrder, setIsReferralInAscendingOrder] =
+    useState(false);
 
-	const toggleRoyaltyPoints = () => {
-		setUserData(sortByRoyaPoints(data));
-		setIsRoyaltyPointInAscendingOrder((prevState) => !prevState);
-	};
+  const toggleRoyaltyPoints = () => {
+    setUserData(sortByRoyaPoints(data));
+    setIsRoyaltyPointInAscendingOrder((prevState) => !prevState);
+  };
 
-	const toggleContestPoints = () => {
-		let url = "https://kaze-api.onrender.com/api/points";
+  const toggleContestPoints = () => {
+    let url = "https://kaze-api.onrender.com/api/points";
 
-		let options = {
-			method: "GET",
-			headers: { "User-Agent": "insomnia/8.4.5" },
-		};
+    let options = {
+      method: "GET",
+      headers: { "User-Agent": "insomnia/8.4.5" },
+    };
 
-		fetch(url, options)
-			.then((res) => res.json())
-			.then((json) => {
-				setUserData(sortByContestPoints(json.data));
-			})
-			.catch((err) => console.error("error:" + err));
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => {
+        setUserData(sortByContestPoints(json.data));
+      })
+      .catch((err) => console.error("error:" + err));
 
-		setIsContestPointInAscendingOrder((prevState) => !prevState);
-	};
+    setIsContestPointInAscendingOrder((prevState) => !prevState);
+  };
 
-	const toggleReferral = () => {
-		setUserData(sortByReferral(data));
-		setIsReferralInAscendingOrder((prevState) => !prevState);
-	};
+  const toggleReferral = () => {
+    setUserData(sortByReferral(data));
+    setIsReferralInAscendingOrder((prevState) => !prevState);
+  };
 
-	useEffect(() => {
-		setUserData(sortByReferral(data));
-	}, []);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 10;
 
-	return userData ? (
-		<div className="w-full">
-			<div className="grid grid-cols-1 mb-24 overflow-x-auto w-full text-left">
-				<table className=" bg-[#111]">
-					<thead className="bg-[#26272B]">
-						<tr className="font-aspekta-medium">
-							<th
-								data-aos="fade-in"
-								className="text-[#D0D5DD] text-xs lg:text-[1.19rem]"
-								//   id="telegram-username"
-							>
-								TELEGRAM USERNAME
-							</th>
-							<th
-								data-aos="fade-in"
-								className="text-[#D0D5DD] text-xs lg:text-[1.19rem] cursor-pointer"
-								id="referrals"
-								onClick={toggleReferral}
-							>
-								REFERRALS
-								<i
-									className={`ml-1 ${
-										isReferralInAscendingOrder
-											? "ri-arrow-down-s-line"
-											: "ri-arrow-up-s-line"
-									}`}
-								></i>
-							</th>
-							<th
-								data-aos="fade-in"
-								className="text-[#D0D5DD] lg:whitespace-nowrap text-xs lg:text-[1.19rem] cursor-pointer"
-								id="royalty-points"
-								onClick={toggleRoyaltyPoints}
-							>
-								ROYALTY POINTS
-								<i
-									className={`ml-1 ${
-										isRoyaltyPointInAscendingOrder
-											? "ri-arrow-down-s-line"
-											: "ri-arrow-up-s-line"
-									}`}
-								></i>
-							</th>
-							<th
-								data-aos="fade-in"
-								className="text-[#D0D5DD] lg:whitespace-nowrap text-xs lg:text-[1.19rem] cursor-pointer"
-								id="contest-points"
-								onClick={toggleContestPoints}
-							>
-								CONTEST POINTS
-								<i
-									className={`ml-1 ${
-										isContestPointInAscendingOrder
-											? "ri-arrow-down-s-line"
-											: "ri-arrow-up-s-line"
-									}`}
-								></i>
-							</th>
-						</tr>
-					</thead>
-					<tbody className="mt-[1.19rem] text-sm w-fit">
-						{userData?.length > 0 ? (
-							userData?.map((d: any, index: any) => (
-								<tr key={index} className="w-fit">
-									<th
-										data-aos="fade-in"
-										className="md:text-xl xl:text-2xl font-aspekta-light-250 w-fit flex items-center gap-2 md:gap-8"
-									>
-										<span className="text-[#FEF1A7] font-aspekta-bold-800">
-											{index + 1}.
-										</span>
-										<span className="flex items-center gap-3 md:gap-5">
-											{index === 0 && (
-												<div className="w-8 hidden lg:inline-block">
-													<Image
-														//   placeholder="blur"
-														width={48}
-														height={48}
-														src="/assets/gold-circle.svg"
-														alt="circle"
-													/>
-												</div>
-											)}
-											{index === 1 && (
-												<div className="w-8 hidden lg:inline-block">
-													<Image
-														//   placeholder="blur"
-														width={48}
-														height={48}
-														src="/assets/silver-circle.svg"
-														alt="circle"
-													/>
-												</div>
-											)}
-											{index === 2 && (
-												<div className="w-8 hidden lg:inline-block">
-													<Image
-														//   placeholder="blur"
-														width={48}
-														height={48}
-														src="/assets/circle-3.svg"
-														alt="circle"
-													/>
-												</div>
-											)}
-											{d?.userName}
-										</span>
-									</th>
-									<td
-										className="text-[#98A2B3] font-aspekta-light-250 md:text-xl xl:text-2xl"
-										data-aos="fade-in"
-									>
-										{d?.referralCount}
-									</td>
-									<td
-										className="text-[#98A2B3] font-aspekta-light-250 md:text-xl xl:text-2xl"
-										data-aos="fade-in"
-									>
-										{d?.points}
-									</td>
-									<td
-										className="text-[#98A2B3] font-aspekta-light-250 md:text-xl xl:text-2xl"
-										data-aos="fade-in"
-									>
-										{d.contestP}
-									</td>
-								</tr>
-							))
-						) : (
-							<>DATA NOT FOUND</>
-						)}
-					</tbody>
-				</table>
-			</div>
-			<div className="my-16 flex justify-center items-center gap-8">
-				{/* add this to the button classname for disabled state */}
-				{/* opacity-50 */}
-				<button className="bg-[#98A2B3] hover:bg-[#b7c0d1] h-8 md:h-10 w-8 md:w-10 rounded-full">
-					<i className="ri-arrow-left-s-line font-semibold text-black text-xl"></i>
-				</button>
-				<button className="bg-[#98A2B3] hover:bg-[#b7c0d1] h-8 md:h-10 w-8 md:w-10 rounded-full">
-					<i className="ri-arrow-right-s-line font-semibold text-black text-xl"></i>
-				</button>
-			</div>
-		</div>
-	) : (
-		<div className="my-8 flex justify-center items-center">
-			<span className="table-loader"></span>
-		</div>
-	);
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = userData?.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(userData?.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * itemsPerPage) % userData.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  useEffect(() => {
+    setUserData(sortByReferral(data));
+    console.log(data);
+  }, []);
+
+  return userData ? (
+    <div className="w-full">
+      <div className="grid grid-cols-1 mb-24 overflow-x-auto w-full text-left">
+        <table className=" bg-[#111]">
+          <thead className="bg-[#26272B]">
+            <tr className="font-aspekta-medium">
+              <th
+                data-aos="fade-in"
+                className="text-[#D0D5DD] text-xs lg:text-[1.19rem]"
+                //   id="telegram-username"
+              >
+                TELEGRAM USERNAME
+              </th>
+              <th
+                data-aos="fade-in"
+                className="text-[#D0D5DD] text-xs lg:text-[1.19rem] cursor-pointer"
+                id="referrals"
+                onClick={toggleReferral}
+              >
+                REFERRALS
+                <i
+                  className={`ml-1 ${
+                    isReferralInAscendingOrder
+                      ? "ri-arrow-down-s-line"
+                      : "ri-arrow-up-s-line"
+                  }`}
+                ></i>
+              </th>
+              <th
+                data-aos="fade-in"
+                className="text-[#D0D5DD] lg:whitespace-nowrap text-xs lg:text-[1.19rem] cursor-pointer"
+                id="royalty-points"
+                onClick={toggleRoyaltyPoints}
+              >
+                ROYALTY POINTS
+                <i
+                  className={`ml-1 ${
+                    isRoyaltyPointInAscendingOrder
+                      ? "ri-arrow-down-s-line"
+                      : "ri-arrow-up-s-line"
+                  }`}
+                ></i>
+              </th>
+              <th
+                data-aos="fade-in"
+                className="text-[#D0D5DD] lg:whitespace-nowrap text-xs lg:text-[1.19rem] cursor-pointer"
+                id="contest-points"
+                onClick={toggleContestPoints}
+              >
+                CONTEST POINTS
+                <i
+                  className={`ml-1 ${
+                    isContestPointInAscendingOrder
+                      ? "ri-arrow-down-s-line"
+                      : "ri-arrow-up-s-line"
+                  }`}
+                ></i>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="mt-[1.19rem] text-sm w-full">
+            {userData?.length > 0 ? (
+              currentItems?.map((d: any, index: any) => (
+                <tr key={index} className="">
+                  <th
+                    data-aos="fade-in"
+                    className="md:text-xl xl:text-2xl font-aspekta-light-250 w-fit flex items-center gap-2 md:gap-8"
+                  >
+                    <span className="text-[#FEF1A7] font-aspekta-bold-800">
+                      {index + 1}.
+                    </span>
+                    <span className="flex items-center gap-3 md:gap-5">
+                      {index === 0 && (
+                        <div className="w-8 hidden lg:inline-block">
+                          <Image
+                            //   placeholder="blur"
+                            width={48}
+                            height={48}
+                            src="/assets/gold-circle.svg"
+                            alt="circle"
+                          />
+                        </div>
+                      )}
+                      {index === 1 && (
+                        <div className="w-8 hidden lg:inline-block">
+                          <Image
+                            //   placeholder="blur"
+                            width={48}
+                            height={48}
+                            src="/assets/silver-circle.svg"
+                            alt="circle"
+                          />
+                        </div>
+                      )}
+                      {index === 2 && (
+                        <div className="w-8 hidden lg:inline-block">
+                          <Image
+                            //   placeholder="blur"
+                            width={48}
+                            height={48}
+                            src="/assets/circle-3.svg"
+                            alt="circle"
+                          />
+                        </div>
+                      )}
+                      {d?.userName}
+                    </span>
+                  </th>
+                  <td
+                    className="text-[#98A2B3] font-aspekta-light-250 md:text-xl xl:text-2xl"
+                    data-aos="fade-in"
+                  >
+                    {d?.referralCount}
+                  </td>
+                  <td
+                    className="text-[#98A2B3] font-aspekta-light-250 md:text-xl xl:text-2xl"
+                    data-aos="fade-in"
+                  >
+                    {d?.points}
+                  </td>
+                  <td
+                    className="text-[#98A2B3] font-aspekta-light-250 md:text-xl xl:text-2xl"
+                    data-aos="fade-in"
+                  >
+                    {d.contestP}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <>DATA NOT FOUND</>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="mb-16 flex justify-center items-center gap-8">
+        {/* add this to the button classname for disabled state */}
+        {/* opacity-50 */}
+        {/* <button className="bg-[#98A2B3] hover:bg-[#b7c0d1] h-8 md:h-10 w-8 md:w-10 rounded-full">
+          <i className="ri-arrow-left-s-line font-semibold text-black text-xl"></i>
+        </button>
+        <button className="bg-[#98A2B3] hover:bg-[#b7c0d1] h-8 md:h-10 w-8 md:w-10 rounded-full">
+          <i className="ri-arrow-right-s-line font-semibold text-black text-xl"></i>
+        </button> */}
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< "
+          renderOnZeroPageCount={null}
+          containerClassName={"paginationBtns"}
+          previousLinkClassName={"previousBtn"}
+          nextLinkClassName={"nextBtn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
+      </div>
+    </div>
+  ) : (
+    <div className="my-8 flex justify-center items-center">
+      <span className="table-loader"></span>
+    </div>
+  );
 }
--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -13,9 +13,9 @@ export default async function Leaderboard() {
   return (
     <main
       id="leaderboard"
-      className="px-5 md:px-20 2xl:px-44 text-white mt-32 xl:mt-44 h-full min-h-screen"
+      className="px-5 md:px-20 2xl:px-44 text-white mt-28 xl:mt-44 h-full min-h-screen"
     >
-      <div className="mb-14 lg:mb-20">
+      <div className="mb-12 lg:mb-20">
         <span className="tracking-[-0.05rem] font-aspekta-light-250 text-3xl lg:text-[2.5rem]">
           Leaderboard - <span className="text-[#FEF1A7]">KazeBot</span>
         </span>
