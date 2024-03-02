3a3cde749b7f6f7ee9cf7ba3386024bb56cfbce0
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-03-02 10:19:28 +0100

    feat: create stakingpool component

--- components/StakeData.tsx
+++ components/StakeData.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import StakeDetails from '../app/data.json'
+import StakePool from './StakePool';
 
 interface Stake {
   totalLocked: string;
@@ -23,7 +24,8 @@ const StakeData = () => {
         <div className='text-white  2xl:max-w-[80rem] w-full my-[50px] '>
           <div>
             {stakes.map((stake, index) => (
-              <section key={index} className='grid 2xl:grid-cols-3 grid-cols-1 space-y-[30px] lg:space-y-[0px] '>
+              <section key={index} >
+                <div className='grid 2xl:grid-cols-3 grid-cols-1 space-y-[30px] lg:space-y-[0px] '>
                 <div className='bg-[#0A0F22] p-[40px] rounded-[5px] w-[90%] mx-auto lg:mx-[0px] '>
                   <h3 className='lg:text-[45px] text-[32px]  '>{stake.totalLocked}</h3>
                   <p className='lg:text-[23px] text-[#909AA1] '>Total Value Locked</p>
@@ -36,7 +38,10 @@ const StakeData = () => {
                   <h3 className='lg:text-[45px] text-[32px]  '>{stake.NoStaker}</h3>
                   <p className='lg:text-[23px] text-[#909AA1] '>Number of Stakers</p>
                 </div>
+                </div>
+                <StakePool />
               </section>
+              
             ))}
           </div>
         </div>
---
+++ components/StakePool.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StakePool = () => {
+  return (
+    <main className='flex justify-center mt-[30px]'>
+        <div className='bg-[#0A0F22] lg:w-full w-[95%] rounded-[5px] lg:px-[40px] px-[20px] lg:py-[30px] py-[10px] '>
+            <h3 className='lg:text-[45px] text-[26px] font-bold '>Participate in Kaze Stake</h3>
+
+        </div>
+
+    </main>
+  )
+}
+
+export default StakePool
