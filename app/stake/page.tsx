0c54efc4aac70d08d7161e6d5bcf0e0182dc4bc6
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-03-02 10:05:12 +0100

    feat: import in stakeData

--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar'
+import StakeData from '@/components/StakeData'
 import React from 'react'
 
 const Stake = () => {
@@ -6,14 +7,14 @@ const Stake = () => {
     <>
     {/* <Navbar /> */}
     <section data-aos="fade-in"
-      className="h-[355px] absolute lg:h-[100vh] w-full relative overflow-x-hidden z-0">
-        <div className="h-screen lg:h-[500px] video-container overflow-hidden">
+      className="h-[100vh] absolute lg:h-[100vh] w-full relative overflow-x-hidden z-0">
+        <div className="h-[355px] lg:h-[500px] video-container overflow-hidden">
         <video loop className="" autoPlay muted>
           <source src="/assets/hero-video.mp4" type="video/mp4" />
         </video>
       </div>
-      <div className=' flex justify-center items-center text-[50px] absolute top-1/2 z-0 -translate-y-1/2 left-[49.99%] -translate-x-1/2  '>
-        <div className='relative text-white top-1/2 -translate-y-[250%] max-w-[36rem] lg:max-w-[53rem] 2xl:max-w-[80rem] mx-auto'>
+      <div className=' flex justify-center items-center text-[50px] absolute top-1/2 lg:top-[50%] 2xl:top-[16%] z-0 -translate-y-[215%] 2xl:translate-y-[150%]  lg:translate-y-[245%] left-[49.99%] -translate-x-1/2  '>
+        <div className='relative text-white top-[12%] lg:top-1/2 2xl:translate-y-[4%] -translate-y-[150%] lg:-translate-y-[250%] max-w-[36rem] lg:max-w-[53rem] 2xl:max-w-[80rem] mx-auto'>
         <span className='hero-gradient-text font-extrabold '>
             STAKING
         </span>
@@ -21,7 +22,7 @@ const Stake = () => {
        
 
       </div>
-
+      <StakeData />
     </section>
     </>
   )
